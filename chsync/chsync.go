b363f872db60a01f49493ed3e2a844b7d1737607
Scott Coltharp <scott.coltharp@gmail.com>
2026-06-25 12:12:07 -0500

    syncch

--- chsync/chsync.go
+++ chsync/chsync.go
@@ -2,52 +2,61 @@ package chsync
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func loadTasks(taskCh chan int) {
 	for i := range 25 {
-		time.Sleep(500 * time.Millisecond)
 		fmt.Println("adding task: ", i)
+		time.Sleep(200 * time.Millisecond)
 		taskCh <- i
 	}
 	close(taskCh)
 }
 
-func workTasks(taskCh chan int, done chan bool) {
+func workTasks(id int, taskCh chan int, checkTask chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for item := range taskCh {
-		fmt.Println("working on task: ", item)
-		time.Sleep(800 * time.Millisecond)
+		fmt.Printf("Worker #%d working on task: %d\n", id, item)
+		time.Sleep(1500 * time.Millisecond)
+		checkTask <- item
 	}
-	done <- true
 }
 
-func worker(workdone chan bool) {
-	fmt.Print("working...")
-	time.Sleep(time.Second)
-	fmt.Println("done")
-
-	workdone <- true
-}
-
-func manager(workdone chan bool, managedone chan bool) {
-	<-workdone
-	fmt.Print("Checking employee work...")
-	time.Sleep(time.Second)
-	fmt.Println("done")
-	managedone <- true
+func checkTasks(id int, checkTask chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for item := range checkTask {
+		fmt.Printf("Manager #%d checking task: %d\n", id, item)
+		time.Sleep(500 * time.Millisecond)
+	}
 }
 
 func ChSync() {
-	taskCh := make(chan int, 100)
-	done := make(chan bool)
+	taskCh := make(chan int, 30)
+	checkTask := make(chan int, 30)
+	var workerWg sync.WaitGroup
+	var manageWg sync.WaitGroup
 
 	go loadTasks(taskCh)
-	go workTasks(taskCh, done)
+	for workerID := 1; workerID <= 20; workerID++ {
+		workerWg.Add(1)
+		go workTasks(workerID, taskCh, checkTask, &workerWg)
+	}
+
+	for managerID := 1; managerID <= 10; managerID++ {
+		manageWg.Add(1)
+		go checkTasks(managerID, checkTask, &manageWg)
+	}
+	go func() {
+		workerWg.Wait()
+		close(checkTask)
+	}()
+
 	//workdone := make(chan bool)
 	//managedone := make(chan bool)
 	//go worker(workdone)
 	//go manager(workdone, managedone)
 	//<-managedone
-	<-done
+	manageWg.Wait()
 }
